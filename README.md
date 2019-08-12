2c07e4d1f6bd0742240cd4f795135d8a7803818a
superman-wrdh <13419655360@163.com>
2019-08-12 13:51:46 +0800

    update description

--- README.md
+++ README.md
@@ -2,6 +2,22 @@
 ## 获取数据库表结构信息 并输出到markdown/html/pdf
 ## TODO 输出pdf待实现
 
+## 用法
+* ### 下载本项目
+
+         git clone https://github.com/superman-wrdh/Export-Db-Info.git
+
+* ### 修改db.yaml中数据库信息，改成你自己的数据库
+
+* ### 安装依赖
+         pip3 install -r requirement.txt
+ 
+* ### 运行
+         python main.py -t html
+ 
+         -t 参数 可选为html/markdown/pdf
+
+
 ## 输出markdown 效果如下
 ![输出markdown效果](markdown.png)
 
--- main.py
+++ main.py
@@ -1,6 +1,27 @@
+import argparse
 from core.render_markdown import output_md
 from core.render_html import output_html
 
 if __name__ == '__main__':
-    # output_md()
-    output_html()
+    parser = argparse.ArgumentParser(
+        prog="输出数据库信息到markdown/html/pdf",
+        usage="""
+            python main.py -t [type]
+            """,
+        description="""
+                输出数据库表结构信息到markdown/html/pdf
+            """,
+        epilog="That's all. Thank you for using it ！",
+        add_help=True
+    )
+    parser.add_argument('-t', "--type", default='md', help="输出类型")
+    args = parser.parse_args()
+    output_type = args.type
+    if output_type in ('md', 'markdown', 'MD', 'MARKDOWN'):
+        output_md()
+    elif output_type in ('html', 'HTML'):
+        output_html()
+    elif output_type in ('pdf', 'PDF'):
+        print("TODO 暂未实现")
+    else:
+        print("参数错误")
